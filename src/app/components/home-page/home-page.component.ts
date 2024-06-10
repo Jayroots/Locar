b237cffe3564aa68c66c8ca3a8a4f229cd95eab1
Jérémy Francois [externe] <e00255@agefiph.asso>
2024-06-10 16:47:56 +0200

    recherche par vehicule disponible selon dates OK

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,33 +1,77 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, DatePipe, JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientService } from '../../services/http-client.service';
-import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IVehicule } from '../../interface/IVehicule';
+import dateFormat from 'dateformat';
+
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatDatepickerModule, CommonModule,MatButtonModule],
+  imports: [MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe, CommonModule, DatePipe],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   providers: [provideNativeDateAdapter()]
 })
 export class HomePageComponent {
 
-  laReponse : any;
+  startDate : string = "";
+  endDate : string = "";
+  dateFormatee : string = "";
+  dateDepartAFormater : any;
+  dateFinAFormater : any;
 
-  listeVehiculeComplete : any;
 
-  constructor(private httpService : HttpClientService) {}
+listeVehiculeComplete : IVehicule[] = new Array;
+  
+listeVehiculesParDate : IVehicule[] = new Array;
+
+range = new FormGroup({
+  start: new FormControl<Date | null>(null),
+  end: new FormControl<Date | null>(null),
+});
+
+
+
+
+  constructor(private httpService : HttpClientService) {
+    
+  }
  
-  rechercheVehicules(){
+
+
+
+
+  listeTousLesVehicules(){
     this.httpService.recupererTousLesVehicules().pipe()
     .subscribe(reponse => {
-      this.laReponse = JSON.stringify(reponse);
+      this.listeVehiculeComplete = (reponse);
       console.log(reponse);
     });
+  }
+
+
+  recupererTousLesVehiculesParDate(){
+
+    this.dateDepartAFormater = this.range.value.start;
+this.dateFinAFormater = this.range.value.end;
+
+
+    this.startDate = dateFormat(this.dateDepartAFormater, "yyyy-mm-dd").toString();
+    this.endDate = dateFormat(this.dateFinAFormater, "yyyy-mm-dd").toString();
+  
+      this.httpService.recupererTousLesVehiculesParDate(this.startDate, this.endDate).pipe()
+      .subscribe(vehiculesParDate => {
+        this.listeVehiculesParDate = vehiculesParDate;
+        console.log(vehiculesParDate)
+      }
+
+      )
   }
 
 
--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -15,4 +15,8 @@ url : string = "http://localhost:8080/api"
     return this.http.get<any>(this.url + '/public/vehicules');
   }
 
+  recupererTousLesVehiculesParDate(startDate:string, endDate:string){
+    return this.http.get<any>(this.url + '/public/vehicules?page=0&startDate='+ startDate + '&endDate='+ endDate);
+  }
+
 }
