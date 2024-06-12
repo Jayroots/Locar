bd87612593a5d980e702590f6f5292012f2e7f1f
Jérémy Francois [externe] <e00255@agefiph.asso>
2024-06-12 21:40:25 +0200

    ajout permis dans la page d inscription ok

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IClient } from '../interface/IClient';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,22 @@ url : string = "http://localhost:8080/api"
 
   recupererTousLesVehiculesParDate(startDate:string, endDate:string){
     return this.http.get<any>(this.url + '/public/vehicules?page=0&startDate='+ startDate + '&endDate='+ endDate);
+  }
+
+  recupererVehiculeParId(id:number){
+    return this.http.get<any>(this.url + '/public/vehicules/{id}?idVehicle='+ id)
+  }
+
+
+  connexion(id: string, mdp: string) {
+    let body = { "email" : id, "password" : mdp};
+    return this.http.post<any>(this.url + "/auth/user/authenticate", body);
+  }
+
+  inscription(client : IClient){
+    let body = client;
+
+    return this.http.post<any>(this.url + "/public/register", body)
   }
 
 }
